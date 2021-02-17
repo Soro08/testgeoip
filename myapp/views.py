3e79e570b1bc71c69604e64c6a1c7a687518f2c7
Soro08 <soro@nan.ci>
2021-02-17 12:51:12 +0000

    add result page

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from geopy.geocoders import Nominatim
 
 # Create your views here.
 
 def home(request):
+    data = {
+        'countrycode':'fr',
+    }
+    return render(request, 'index.html', data)
 
 
-    return render(request, 'index.html')
+def result(request):
+    if request.method == 'POST':
+        lieu = request.POST.get('location')
+        geolocator = Nominatim(user_agent="test")
+        location = geolocator.geocode(lieu)
+        print(location)
+
+        data = {
+            'long': location.longitude,
+            'lat': location.latitude,
+            'add': location.address
+        }
+
+    return render(request, 'result.html', data)
