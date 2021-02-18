830d5c3ac2d6771f611600c5081feb7759657544
Soro08 <soro@nan.ci>
2021-02-18 17:23:02 +0000

    update

--- coolfund/urls.py
+++ coolfund/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.show_genres),
+
+
+    path('api', views.getuseripinfo),
 ]
--- coolfund/views.py
+++ coolfund/views.py
@@ -3,3 +3,9 @@ from . import models
 # Create your views here.
 def show_genres(request):
     return render(request, "cool.html", {'genres': models.Genre.objects.all()})
+
+
+def getuseripinfo(request):
+
+
+    return render(request, 'ip/index.html', locals())
